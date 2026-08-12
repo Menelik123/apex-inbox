1e1cc57f47274b761c6935a6a0de0caf0e02720b
Menelik Garrick <menelikgarrick@gmail.com>
2026-08-11 21:22:33 -0400

    Fix callbacks merge conflict between auth.config and auth.ts

--- auth.ts
+++ auth.ts
@@ -6,7 +6,6 @@ import NextAuth, { type DefaultSession } from "next-auth";
 import { prisma } from "@/lib/db";
 import { getUserById } from "@/lib/user";
 
-// More info: https://authjs.dev/getting-started/typescript#module-augmentation
 declare module "next-auth" {
   interface Session {
     user: {
@@ -19,49 +18,33 @@ export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
+  ...authConfig,
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   pages: {
     signIn: "/login",
-    // error: "/auth/error",
   },
   callbacks: {
+    ...authConfig.callbacks,
     async session({ token, session }) {
       if (session.user) {
-        if (token.sub) {
-          session.user.id = token.sub;
-        }
-
-        if (token.email) {
-          session.user.email = token.email;
-        }
-
-        if (token.role) {
-          session.user.role = token.role;
-        }
-
+        if (token.sub) session.user.id = token.sub;
+        if (token.email) session.user.email = token.email;
+        if (token.role) session.user.role = token.role as UserRole;
         session.user.name = token.name;
-        session.user.image = token.picture;
+        session.user.image = token.picture as string;
       }
-
       return session;
     },
-
     async jwt({ token }) {
       if (!token.sub) return token;
-
       const dbUser = await getUserById(token.sub);
-
       if (!dbUser) return token;
-
       token.name = dbUser.name;
       token.email = dbUser.email;
       token.picture = dbUser.image;
       token.role = dbUser.role;
-
       return token;
     },
   },
-  ...authConfig,
-  // debug: process.env.NODE_ENV !== "production"
 });
